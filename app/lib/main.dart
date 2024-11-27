3e1718ef26ba4c5cf2fa9b7e8b8c5027067480df
AyaAbdElmoneim158 <ayaabdelmon@gmail.com>
2024-11-27 10:35:27 +0200

    ADD: Device orientation

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,10 +3,17 @@ import 'package:app/core/app/env.variables.dart';
 import 'package:app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await EnvVariable.instance.init(envType: EnvType.dev);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const AsrooStoreApp());
+
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then(
+    (_) => runApp(const AsrooStoreApp()),
+  );
 }
